b1fbbd4d3f27ccc4930212996ed713eddb4a8432
Tracy Ngo <tngo1206@gmail.com>
2019-09-14 18:08:09 -0700

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,14 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
+let element = document.createElement('div');
+//create the element
+
+element.innerHTML = 'Hello, DOM!';
+element.style.backgroundColor = '#f9f9f9';
+//color and add content
+
+document.body.appendChild(element);
+//add to the DOM
+
+element.style.textAlign = 'center';
+//align now visible text
